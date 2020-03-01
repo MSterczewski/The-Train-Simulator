3721256431c1bdf76557cbf0fcfb50286de30552
xdab <xdab@nie>
2020-03-01 16:30:51 +0100

    dict<neigId,Edge> to list<Edge> allows multigraphs

--- Graphs/Node.cs
+++ Graphs/Node.cs
@@ -12,25 +12,24 @@ namespace Graphs
     {
         public int Id { get; private set; }
 
-        // Ce n'est pas un dictionnaire ~ Rene Magritte, 1929
-        private Dictionary<int, Edge> Neighbours;//NOTE: change this to Dict<Node,Edge>
-                                                 //NOTE: does this work for multigraphs?
+        
+        public List<Edge> OutgoingEdges;
 
 
         // Almost a method
         public int Degree
-           => this.Neighbours.Count;
+           => this.OutgoingEdges.Count;
 
 
         public Node(int Id)
         {
             this.Id = Id;
-            this.Neighbours = new Dictionary<int, Edge>();
+            this.OutgoingEdges = new List<Edge>();
         }
 
 
-        public bool IsNeighbour(int NodeId)//NOTE: if change into Dict<Node> this neads to be changed
-            => this.Neighbours.ContainsKey(NodeId);
+        public bool IsNeighbour(int NodeId) //NOTE: if change into Dict<Node> this neads to be changed
+            => this.OutgoingEdges.Any(edge => (edge.Destination == NodeId));
 
 
         // Single-directional connection only
@@ -42,17 +41,8 @@ namespace Graphs
             e.Color = Color;
             e.Weight = Weight;
 
-            this.Neighbours[NeighbourId] = e;//NO
-            //this.Neighbours.Add()
+            this.OutgoingEdges.Add(e);
         }
-
-
-        // Workaround allowing iteration over neighbours, 
-        // whereas GetEnumerator would create an illusion
-        // of the edged being somehow a part of a vertice
-        public Dictionary<int, Edge> GetNeighbours()//NOTE: Why this when there is a variable?
-            => this.Neighbours;
-
     }
 }
 }
